035be303954e034d41e2280e4a7876533be7ed44
Darin Ivanov <d.v.ivanov98@gmail.com>
2023-03-14 21:38:26 +0200

    recurison doesnt cut off

--- machine_learning/decision_tree_binary.py
+++ machine_learning/decision_tree_binary.py
@@ -7,9 +7,10 @@ class Tree:
 
     def __init__(self,feature,value=None):
         self.feature = feature
-        self.value = value
         self.left_child = None
         self.right_child = None
+        #if val then it's leaf node
+        self.value = value
 
     def build_tree(self,df):
         y_val = df.columns[-1]
@@ -21,18 +22,21 @@ class Tree:
         if len(df)<=20 and len(df[df[y_val]==0])!=len(df[df[y_val]==1]):
             self.value = sorted(df[y_val].value_counts().idxmax())
             return
+        if len(df)<=20:
+            self.value = 0
+            return
         n_subset = df[df[self.feature]==0]
         p_subset = df[df[self.feature]==1]
-        n_best = Tree.pick_best_feature(n_subset)
-        p_best = Tree.pick_best_feature(p_subset)
+        n_best = Tree.pick_best_feature(n_subset,self.feature)
+        p_best = Tree.pick_best_feature(p_subset,self.feature)
         self.left_child = Tree(n_best).build_tree(n_subset)
         self.right_child = Tree(p_best).build_tree(p_subset)
 
     @staticmethod
-    def pick_best_feature(df):
+    def pick_best_feature(df,previos_best):
 
         def calculate_entropy(df_filtered):
-            p = len(df_filtered[df_filtered[df_filtered.columns[-1]]==1])/len(df_filtered)
+            p = len(df_filtered[df_filtered[df_filtered.columns[-1]]==1])/len(df_filtered) if len(df_filtered)!=0 else 0
             entropy = - p*math.log(p+0.000001,2) - (1-p)*math.log((1-p)+0.000001,2)
             return entropy
     
@@ -40,11 +44,13 @@ class Tree:
     
         for col in list(df.columns)[:-1]:
             #calculate dependent variable entropy for explanatory feature being possitive/negative
+            if col == previos_best:
+                continue
             p_exmp = df[df[col]==1]
             n_exmp = df[df[col]==0]
             p_entropy = calculate_entropy(p_exmp) 
             n_entropy = calculate_entropy(n_exmp)
-            p = len(p_exmp)/len(df)
+            p = len(p_exmp)/len(df) if len(p_exmp)!=0 else 1/2
             avg_entropy = p*p_entropy + (1-p)*n_entropy
             entropyes[col] = avg_entropy
         #fix this so it gets a random feature if more than one min features.
@@ -58,7 +64,7 @@ def test():
     df_train = df[['Held_Open','Trend_bool','RVOL_bool','Gap_bool','ExCl','D2']][:int(len(df)*0.8)]
     df_test = df[['Held_Open','Trend_bool','RVOL_bool','Gap_bool','ExCl','D2']][int(len(df)*0.8):].values.tolist()
     # print(df_train['ExCl'].value_counts().idxmax())
-    feature = Tree.pick_best_feature(df_train)
+    feature = Tree.pick_best_feature(df_train,None)
     root = Tree(feature)
     root.build_tree(df_train)
     print(root.__class__)
